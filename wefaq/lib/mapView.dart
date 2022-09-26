aaa62503194d7cbb6d31f34be775c6abc43b3526
Alhanouf <79309859+Alhanoufala@users.noreply.github.com>
2022-09-27 02:48:46 +0300

    map view (adding search )

--- wefaq/lib/mapView.dart
+++ wefaq/lib/mapView.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_place/google_place.dart';
 
 import 'bottom_bar_custom.dart';
 
@@ -17,6 +18,8 @@ class MapSampleState extends State<MapSample> {
   @override
   void initState() {
     getMarkers();
+    String apiKey = 'AIzaSyCkRaPfvVejBlQIAWEjc9klnkqk6olnhuc';
+    googlePlace = GooglePlace(apiKey);
 
     super.initState();
   }
@@ -28,6 +31,14 @@ class MapSampleState extends State<MapSample> {
 
   final _firestore = FirebaseFirestore.instance;
   List<Marker> markers = [];
+  static final TextEditingController _startSearchFieldController =
+      TextEditingController();
+
+  DetailsResult? startPosition;
+
+  late GooglePlace googlePlace;
+  List<AutocompletePrediction> predictions = [];
+  Timer? _debounce;
 
   //get markers
   Future getMarkers() async {
@@ -43,6 +54,24 @@ class MapSampleState extends State<MapSample> {
       }
   }
 
+  changeLocation() async {
+    final GoogleMapController controller = await _controller.future;
+    double? lat = startPosition?.geometry?.location?.lat;
+    double? lng = startPosition?.geometry?.location?.lng;
+    controller.animateCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(target: LatLng(lat!, lng!), zoom: 14)));
+    _startSearchFieldController.clear();
+  }
+
+  void autoCompleteSearch(String value) async {
+    var result = await googlePlace.autocomplete.get(value);
+    if (result != null && result.predictions != null && mounted) {
+      setState(() {
+        predictions = result.predictions!;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -54,13 +83,105 @@ class MapSampleState extends State<MapSample> {
         currentHomeScreen: 1,
         updatePage: () {},
       ),
-      body: GoogleMap(
-        mapType: MapType.normal,
-        markers: markers.toSet(),
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-        initialCameraPosition: _kGooglePlex,
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 5, left: 1, right: 1),
+            child: TextFormField(
+              controller: _startSearchFieldController,
+              decoration: InputDecoration(
+                  hintText: 'Ksu..auto complete',
+                  hintStyle: TextStyle(
+                      fontSize: 16, color: Color.fromARGB(255, 202, 198, 198)),
+                  label: RichText(
+                    text: TextSpan(
+                      text: 'Search ',
+                      style: const TextStyle(
+                          fontSize: 18, color: Color.fromARGB(144, 64, 7, 87)),
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black87, width: 2.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color.fromARGB(144, 64, 7, 87),
+                      width: 2.0,
+                    ),
+                  ),
+                  suffixIcon: _startSearchFieldController.text.isEmpty
+                      ? Icon(Icons.search,
+                          color: Color.fromARGB(221, 137, 171, 187))
+                      : IconButton(
+                          icon: Icon(Icons.search,
+                              color: Color.fromARGB(221, 137, 171, 187)),
+                          onPressed: () {
+                            changeLocation();
+                          },
+                        )),
+              onChanged: (value) {
+                if (_debounce?.isActive ?? false) _debounce!.cancel();
+                _debounce = Timer(const Duration(milliseconds: 1000), () {
+                  if (value.isNotEmpty) {
+                    //places api
+                    autoCompleteSearch(value);
+                  } else {
+                    //clear out the results
+                    setState(() {
+                      predictions = [];
+                      startPosition = null;
+                    });
+                  }
+                });
+              },
+            ),
+          ),
+          Scrollbar(
+            thumbVisibility: true,
+            child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: predictions.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Color.fromARGB(221, 137, 171, 187),
+                      child: Icon(
+                        Icons.pin_drop,
+                        color: Colors.white,
+                      ),
+                    ),
+                    title: Text(
+                      predictions[index].description.toString(),
+                    ),
+                    onTap: () async {
+                      final placeId = predictions[index].placeId!;
+                      final details = await googlePlace.details.get(placeId);
+                      if (details != null &&
+                          details.result != null &&
+                          mounted) {
+                        setState(() {
+                          startPosition = details.result;
+                          _startSearchFieldController.text =
+                              details.result!.name!;
+
+                          predictions = [];
+                        });
+                      }
+                    },
+                  );
+                }),
+          ),
+          Expanded(
+            child: GoogleMap(
+              mapType: MapType.normal,
+              markers: markers.toSet(),
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+              initialCameraPosition: _kGooglePlex,
+            ),
+          ),
+        ],
       ),
     );
   }
