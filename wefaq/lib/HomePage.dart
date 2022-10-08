1f9797025dc8f4a47ccb69e75a744b4c1268e9b2
SarahIbra <98983635+SAlShathri@users.noreply.github.com>
2022-10-09 01:40:30 +0300

    fixed

--- wefaq/lib/HomePage.dart
+++ wefaq/lib/HomePage.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:wefaq/favoriteProject.dart';
+//import 'package:wefaq/favoriteProject.dart';
 import 'package:wefaq/profile.dart';
 import 'RJRprojects.dart';
 import 'package:wefaq/backgroundHome.dart';
