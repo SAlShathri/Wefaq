78557d1f202fea9612afb223981c7eff63325ecd
nalsomali <naadaalsomali@gmail.com>
2022-10-21 17:09:29 +0300

    Added log out confirmation message

--- wefaq/lib/HomePage.dart
+++ wefaq/lib/HomePage.dart
@@ -20,6 +20,10 @@ class HomeScreen extends StatefulWidget {
   HomeScreenState createState() => HomeScreenState();
 }
 
+Future<void> _signOut() async {
+  await FirebaseAuth.instance.signOut();
+}
+
 //,,,
 class HomeScreenState extends State<HomeScreen> {
   final auth = FirebaseAuth.instance;
@@ -30,10 +34,6 @@ class HomeScreenState extends State<HomeScreen> {
     getProjectTitle();
     getProjectTitleOwner();
     super.initState();
-  }
-
-  Future<void> _signOut() async {
-    await FirebaseAuth.instance.signOut();
   }
 
   static List<String> ProjectTitleList = [];
@@ -125,9 +125,7 @@ class HomeScreenState extends State<HomeScreen> {
                       color: Color.fromARGB(255, 255, 255, 255),
                     ),
                     onPressed: () {
-                      _signOut();
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => UserLogin()));
+                      showDialogFunc(context);
                     }),
               ),
               SizedBox(
@@ -269,4 +267,150 @@ class CategoryCard extends StatelessWidget {
       ),
     );
   }
+}
+
+showDialogFunc(context) {
+  return showDialog(
+      context: context,
+      builder: (context) {
+        return Center(
+            child: Material(
+                type: MaterialType.transparency,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                  ),
+                  padding: const EdgeInsets.all(15),
+                  height: 150,
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      // Code for acceptance role
+                      Row(children: <Widget>[
+                        Expanded(
+                          flex: 2,
+                          child: GestureDetector(
+                            child: Text(
+                              " Are you sure you want to log out? ",
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Color.fromARGB(159, 64, 7, 87),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            onTap: () {
+                              // go to participant's profile
+                            },
+                          ),
+                        ),
+                        // const SizedBox(
+                        //   height: 10,
+                        // ),
+                      ]),
+                      SizedBox(
+                        height: 35,
+                      ),
+                      //----------------------------------------------------------------------------
+                      Row(
+                        children: <Widget>[
+                          Text("   "),
+                          Text("     "),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => HomeScreen()));
+                            },
+                            style: ElevatedButton.styleFrom(
+                              surfaceTintColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(80.0)),
+                              padding: const EdgeInsets.all(0),
+                            ),
+                            child: Container(
+                              alignment: Alignment.center,
+                              height: 40.0,
+                              width: 100,
+                              decoration: new BoxDecoration(
+                                  borderRadius: BorderRadius.circular(9.0),
+                                  gradient: new LinearGradient(colors: [
+                                    Color.fromARGB(144, 176, 175, 175),
+                                    Color.fromARGB(144, 176, 175, 175),
+                                  ])),
+                              padding: const EdgeInsets.all(0),
+                              child: Text(
+                                "Cancel",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                    color: Color.fromARGB(255, 255, 255, 255)),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 40),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                _signOut();
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => UserLogin()));
+                                // CoolAlert.show(
+                                //   context: context,
+                                //   title: "Success!",
+                                //   confirmBtnColor:
+                                //       Color.fromARGB(144, 64, 6, 87),
+                                //   type: CoolAlertType.success,
+                                //   backgroundColor:
+                                //       Color.fromARGB(221, 212, 189, 227),
+                                //   text: "You have logged out successfully",
+                                //   confirmBtnText: 'Done',
+                                //   onConfirmBtnTap: () {
+                                //     //send join requist
+                                //     _signOut();
+                                //     Navigator.push(
+                                //         context,
+                                //         MaterialPageRoute(
+                                //             builder: (context) => UserLogin()));
+                                //   },
+                                // );
+                              },
+                              style: ElevatedButton.styleFrom(
+                                surfaceTintColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(80.0)),
+                                padding: const EdgeInsets.all(0),
+                              ),
+                              child: Container(
+                                alignment: Alignment.center,
+                                height: 40.0,
+                                width: 100,
+                                decoration: new BoxDecoration(
+                                    borderRadius: BorderRadius.circular(9.0),
+                                    gradient: new LinearGradient(colors: [
+                                      Color.fromARGB(144, 210, 2, 2),
+                                      Color.fromARGB(144, 210, 2, 2)
+                                    ])),
+                                padding: const EdgeInsets.all(0),
+                                child: Text(
+                                  "Log out",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                      color:
+                                          Color.fromARGB(255, 255, 255, 255)),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )));
+      });
 }
--- wefaq/lib/chats.dart
+++ wefaq/lib/chats.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cool_alert/cool_alert.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
@@ -15,6 +16,10 @@ class chatScreen extends StatefulWidget {
   _chatScreenState createState() => _chatScreenState();
 }
 
+Future<void> _signOut() async {
+  await FirebaseAuth.instance.signOut();
+}
+
 class _chatScreenState extends State<chatScreen> {
   final auth = FirebaseAuth.instance;
   late User signedInUser;
@@ -29,10 +34,6 @@ class _chatScreenState extends State<chatScreen> {
   void initState() {
     getLastMessage();
     super.initState();
-  }
-
-  Future<void> _signOut() async {
-    await FirebaseAuth.instance.signOut();
   }
 
   var fillterd;
@@ -83,9 +84,7 @@ class _chatScreenState extends State<chatScreen> {
                 color: Color.fromARGB(255, 255, 255, 255),
               ),
               onPressed: () {
-                _signOut();
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => UserLogin()));
+                showDialogFunc(context);
               }),
         ],
       ),
@@ -244,4 +243,150 @@ class _chatScreenState extends State<chatScreen> {
       ),
     );
   }
+}
+
+showDialogFunc(context) {
+  return showDialog(
+      context: context,
+      builder: (context) {
+        return Center(
+            child: Material(
+                type: MaterialType.transparency,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                  ),
+                  padding: const EdgeInsets.all(15),
+                  height: 150,
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      // Code for acceptance role
+                      Row(children: <Widget>[
+                        Expanded(
+                          flex: 2,
+                          child: GestureDetector(
+                            child: Text(
+                              " Are you sure you want to log out? ",
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Color.fromARGB(159, 64, 7, 87),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            onTap: () {
+                              // go to participant's profile
+                            },
+                          ),
+                        ),
+                        // const SizedBox(
+                        //   height: 10,
+                        // ),
+                      ]),
+                      SizedBox(
+                        height: 35,
+                      ),
+                      //----------------------------------------------------------------------------
+                      Row(
+                        children: <Widget>[
+                          Text("   "),
+                          Text("     "),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => chatScreen()));
+                            },
+                            style: ElevatedButton.styleFrom(
+                              surfaceTintColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(80.0)),
+                              padding: const EdgeInsets.all(0),
+                            ),
+                            child: Container(
+                              alignment: Alignment.center,
+                              height: 40.0,
+                              width: 100,
+                              decoration: new BoxDecoration(
+                                  borderRadius: BorderRadius.circular(9.0),
+                                  gradient: new LinearGradient(colors: [
+                                    Color.fromARGB(144, 176, 175, 175),
+                                    Color.fromARGB(144, 176, 175, 175),
+                                  ])),
+                              padding: const EdgeInsets.all(0),
+                              child: Text(
+                                "Cancel",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                    color: Color.fromARGB(255, 255, 255, 255)),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 40),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                _signOut();
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => UserLogin()));
+                                // CoolAlert.show(
+                                //   context: context,
+                                //   title: "Success!",
+                                //   confirmBtnColor:
+                                //       Color.fromARGB(144, 64, 6, 87),
+                                //   type: CoolAlertType.success,
+                                //   backgroundColor:
+                                //       Color.fromARGB(221, 212, 189, 227),
+                                //   text: "You have logged out successfully",
+                                //   confirmBtnText: 'Done',
+                                //   onConfirmBtnTap: () {
+                                //     //send join requist
+                                //     _signOut();
+                                //     Navigator.push(
+                                //         context,
+                                //         MaterialPageRoute(
+                                //             builder: (context) => UserLogin()));
+                                //   },
+                                // );
+                              },
+                              style: ElevatedButton.styleFrom(
+                                surfaceTintColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(80.0)),
+                                padding: const EdgeInsets.all(0),
+                              ),
+                              child: Container(
+                                alignment: Alignment.center,
+                                height: 40.0,
+                                width: 100,
+                                decoration: new BoxDecoration(
+                                    borderRadius: BorderRadius.circular(9.0),
+                                    gradient: new LinearGradient(colors: [
+                                      Color.fromARGB(144, 210, 2, 2),
+                                      Color.fromARGB(144, 210, 2, 2)
+                                    ])),
+                                padding: const EdgeInsets.all(0),
+                                child: Text(
+                                  "Log out",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                      color:
+                                          Color.fromARGB(255, 255, 255, 255)),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )));
+      });
 }
